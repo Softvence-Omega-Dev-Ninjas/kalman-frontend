c480e7e456b7bb0ac422df49dafe1b3cf2593d82
Haniful Islam <haniful360.com>
2025-10-24 03:46:05 +0600

    add send proposal modal

--- src/components/Jobs/JobDetails/JobInformaiton.tsx
+++ src/components/Jobs/JobDetails/JobInformaiton.tsx
@@ -9,33 +9,33 @@ const JobInformation = ({ job }: { job: any }) => {
       <div className="flex items-center justify-between gap-6 mb-5 bg-white p-5 rounded-md shadow-md">
         <div className="space-y-3">
           <div className="flex items-center gap-3">
-            <h1 className="text-2xl font-semibold">{job.title}</h1>
+            <h1 className="text-2xl font-semibold">{job?.title}</h1>
             {job.tag === "Urgent" && (
               <span className="px-3 py-1 text-sm rounded-md bg-red-100 text-red-500">
-                {job.tag}
+                {job?.tag}
               </span>
             )}
           </div>
           <div className="flex items-center gap-3 text-secondary">
             <div className="flex items-center gap-2">
               <LuCalendarDays />
-              <p>{job.date}</p>
+              <p>{job?.date}</p>
             </div>
             <div className="flex items-center gap-2">
               <WiTime4 className="text-lg" />
-              <p>{job.start}</p>
+              <p>{job?.start}</p>
             </div>
           </div>
         </div>
         <div className="text-secondary space-y-3">
-          <h1 className="text-2xl font-semibold">{job.rate}</h1>
+          <h1 className="text-2xl font-semibold">{job?.rate}</h1>
           <p className="text-lg">Hourly Budget</p>
         </div>
       </div>
       <div className="bg-white space-y-10 p-5 rounded-md shadow-md">
         <div>
           <h1 className="text-2xl font-semibold mb-3">Job Description</h1>
-          <p className="text-secondary text-sm">{job.description}</p>
+          <p className="text-secondary text-sm">{job?.description}</p>
         </div>
         <div>
           <h1 className="text-xl font-semibold mb-3">Timeline</h1>
