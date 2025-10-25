262f52c245494ed11335d86578c2dc5ca90194d8
habibb2r <habibhk127@gmail.com>
2025-10-26 05:22:56 +0600

    Feat: Chat Functionaliy COmpleted

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,15 +4,18 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://pravaruka.sk", //  Change this to your API base URL
+    // baseUrl: "https://pravaruka.sk", //  Change this to your API base URL
+    baseUrl: "http://10.10.10.54:7000", //  Change this to your API base URL
     prepareHeaders: (headers, { getState }) => {
 
      const state = getState() as any;
-     const token = state.admin?.token || state.user?.token;
+     console.log("Current state in prepareHeaders:", state);
+     const token = state.auth?.token;
+     console.log("Preparing headers with token:", token);
 
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
-        console.log(token)
+        console.log("Authorization header set:", token);
       }
       return headers;
     },
@@ -26,7 +29,8 @@ export const baseApi = createApi({
     "Dasboard",
     "Blogs",
     "Contact",
-     "Admin",
+    "Admin",
+    "Chat",
   ],
 
   endpoints: () => ({}),
---
+++ src/redux/features/message/messageApi.ts
@@ -0,0 +1,38 @@
+// src/redux/features/auth/authApi.ts
+import { baseApi } from "@/redux/api/baseApi";
+
+export const messageApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    
+    getChatList: builder.query({
+      query: () => ({
+        url: "/chat/partners",
+        method: "GET",
+      }),
+      providesTags: ["Chat"],
+    }),
+
+    getChatHistory: builder.query({
+      query: ({userA, userB}) => ({
+        url: `/chat/history/${userA}/${userB}`,
+        method: "GET",
+      }),
+      providesTags: ["Chat"],
+    }),
+
+    adminLogin: builder.mutation({
+      query: (data) => ({
+        url: "/auth/admin-login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+
+  useGetChatListQuery,
+    useGetChatHistoryQuery,
+  useAdminLoginMutation
+} = messageApi;
