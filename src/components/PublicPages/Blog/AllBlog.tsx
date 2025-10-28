3fc979fe9d6bdb89e08992b72f9b384542d0cbdf
Niloy Roy <niloyroyaiub@gmail.com>
2025-10-28 06:17:57 +0600

    blog lenght error fixed

--- src/components/PublicPages/Blog/AllBlog.tsx
+++ src/components/PublicPages/Blog/AllBlog.tsx
@@ -24,7 +24,7 @@ const AllBlog: React.FC<AllBlogsProps> = ({ blog }) => {
         </div>
 
         {/* Button */}
-        {blog.length > 0 && (
+        {blog?.length > 0 && (
           <div className="flex justify-center mt-10">
             <button className="bg-[#FF6B35] text-white px-18 py-3 rounded-md font-medium hover:bg-[#e65a29] transition">
               See all
